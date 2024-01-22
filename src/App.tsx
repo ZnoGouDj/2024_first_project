d4f51fcd26d5dbb0afb04a6b77d7e8b490f9fff4
znogoud <znogoud3@gmail.com>
2024-01-22 16:55:38 +0100

    add global styles, add dark/light theme application

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router-dom";
-import './index.scss';
+import './styles/index.scss';
 import { Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { Suspense } from "react";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE THEME</button>
             <Link to={'/'}>Main page</Link>
             <Link to={'/about'}>About website</Link>
             <Suspense fallback={<div>Loading...</div>}>
