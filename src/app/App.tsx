c5047ba73dfc5337c336f9e6a6bca87235611a89
znogoud <znogoud3@gmail.com>
2024-01-25 16:34:03 +0100

    refactoring, move navbar to widgets folder, add themes, extend Link component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 import { useTheme } from "app/providers/ThemeProvider/lib/useTheme";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppRouter } from "./providers/router";
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE THEME</button>
-            <Link to={'/'}>Main page</Link>
-            <Link to={'/about'}>About website</Link>
+            <Navbar />
             <AppRouter />
+            <button onClick={toggleTheme}>TOGGLE THEME</button>
         </div>
     )
 }
