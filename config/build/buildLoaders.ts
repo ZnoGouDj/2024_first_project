ad604aa91132937af0e2fe036e80a5e8e9270ad4
znogoud <znogoud3@gmail.com>
2024-01-26 16:19:18 +0100

    add svg / png support for ts, add icons for themes, add custom button component

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,10 @@ import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    }
 
     const cssLoader = {
         test: /\.s[ac]ss$/i,
@@ -30,7 +34,18 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         exclude: /node_modules/,
     }
 
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+    }
+
     return [
+        fileLoader,
+        svgLoader,
         typescriptLoader, 
         cssLoader,
     ]
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,16 @@
 import './styles/index.scss';
-import { Link } from "react-router-dom";
 import { useTheme } from "app/providers/ThemeProvider/lib/useTheme";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppRouter } from "./providers/router";
 import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
-    const {theme, toggleTheme} = useTheme();
+    const {theme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <Navbar />
             <AppRouter />
-            <button onClick={toggleTheme}>TOGGLE THEME</button>
         </div>
     )
 }
