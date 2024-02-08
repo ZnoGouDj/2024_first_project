d3a6403b8c5cea36b1aa175d761d0afc3739ca10
znogoud <znogoud3@gmail.com>
2024-02-08 01:17:44 +0100

    add react testing library, add tests for btn, add sidebar

--- src/app/providers/ErrorBoundary/ui/BugButton.tsx
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -1,4 +1,5 @@
-import Button from 'shared/ui/Button/Button';
+import React from 'react';
+import Button from '../../../../shared/ui/Button/Button';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,8 +1,10 @@
+import React from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Sidebar.module.scss';
 import { useState } from 'react';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher';
+import Button from '../../../../shared/ui/Button/Button';
 
 interface SidebarProps {
     className?: string;
@@ -16,10 +18,13 @@ const Sidebar = ({className}: SidebarProps) => {
     };
 
     return (
-        <div className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}>
-            <button onClick={onToggle}>
+        <div data-testid="sidebar" className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}>
+            <Button
+                data-testid="sidebar-toggle"
+                onClick={onToggle}
+            >
                 toggle
-            </button>
+            </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
