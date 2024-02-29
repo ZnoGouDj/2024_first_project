c0532d5262bac01110c0c53d26ea87e87381a88b
znogoud <znogoud3@gmail.com>
2024-02-29 14:27:49 +0100

    rm test input

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,19 +1,11 @@
-import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import Input from 'shared/ui/Input/Input';
 
 const MainPage = () => {
     const {t} = useTranslation('main');
-    const [value, setValue] = useState('');
-
-    const onChange = (val: string) => {
-        setValue(val);
-    };
 
     return (
         <div>
             {t('Главная страница')}
-            <Input onChange={onChange} value={value} placeholder={'Enter text'}/>
         </div>
     );
 };
