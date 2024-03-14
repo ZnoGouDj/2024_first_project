af7d035275486bb78c51c28b1348a268e4a35722
Egor Sorokin <znogoud3@gmail.com>
2024-03-14 13:14:46 +0100

    update app router

--- src/entities/User/model/slice/userSlice.ts
+++ src/entities/User/model/slice/userSlice.ts
@@ -3,6 +3,7 @@ import { UserSchema, User } from '../types/user';
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
 const initialState: UserSchema = {
+    _inited: false
 };
 
 export const userSlice = createSlice({
@@ -17,6 +18,7 @@ export const userSlice = createSlice({
             if (user) {
                 state.authData = JSON.parse(user);
             }
+            state._inited = true;
         },
         logout: (state) => {
             state.authData = undefined;
